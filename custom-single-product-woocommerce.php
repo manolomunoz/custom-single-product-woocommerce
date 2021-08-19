a81816b5874fa6507e91a7aa3e9dcbc5159978e4
Manolo <manolo@closemarketing.net>
2021-08-19 20:02:21 +0200

    Add email in file header

--- custom-single-product-woocommerce.php
+++ custom-single-product-woocommerce.php
@@ -12,7 +12,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
  * @package     Custom Single Product Woocommerce
- * @author      Manuel Muñoz Rodríguez
+ * @author      Manuel Muñoz Rodríguez <mmr010496@gmail.com>
  * @copyright   2021
  * @license     GPL-2.0+
  *
